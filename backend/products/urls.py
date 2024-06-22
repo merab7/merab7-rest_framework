827bdce648f0f9da0ff26951d343ea487548cdd4
mdot <mdot@mdot>
2024-06-23 00:00:45 +0200

    permissions

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductDetailAPIview, ProductListCreateAPIView, product_alt_view
-
+from .views import ProductDetailAPIview, ProductListCreateAPIView, ProductDeleteAPIview,  ProductUpdateAPIview
 urlpatterns = [
-    path('<int:pk>/', ProductDetailAPIview.as_view()),
+    path('detail/<int:pk>/', ProductDetailAPIview.as_view()),
+    path('delete/<int:pk>/', ProductDeleteAPIview.as_view()),
+    path('update/<int:pk>/', ProductUpdateAPIview.as_view()),
     path('create/', ProductListCreateAPIView.as_view()),
     
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,15 +1,18 @@
-from rest_framework import generics
+from rest_framework import generics, mixins, permissions, authentication
+from yaml import serialize
 from .models import Products
 from .serializers import ProductsSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-
+from .permissions import IsStaffEditorPermission
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Products.objects.all()
     serializer_class = ProductsSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [IsStaffEditorPermission]
 
 
     def perform_create(self, serializer):
@@ -17,6 +20,16 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 
 
 class ProductDetailAPIview(generics.RetrieveAPIView):
+    queryset =  Products.objects.all()
+    serializer_class  = ProductsSerializer
+    # LOOKUP_FIELD = 'PK'
+
+class ProductDeleteAPIview(generics.DestroyAPIView):
+    queryset =  Products.objects.all()
+    serializer_class  = ProductsSerializer
+    # LOOKUP_FIELD = 'PK'
+
+class ProductUpdateAPIview(generics.UpdateAPIView):
     queryset =  Products.objects.all()
     serializer_class  = ProductsSerializer
     # LOOKUP_FIELD = 'PK'
@@ -30,25 +43,37 @@ class ProductDetailAPIview(generics.RetrieveAPIView):
 # 
 #  
 
-# functiion based view wher is implemanted creation and retrive
-@api_view(["GET", "POST"])
-def product_alt_view(request, pk=None, *args, **kwargs):
-    method  = request.method
-
-    if method == "GET":
-       
-       if pk is not None:
-           obj = get_object_or_404(Products, pk=pk)
-           data = ProductsSerializer(obj).data
-           return Response(data)
-       
-       qs = Products.objects.all()
-       data = ProductsSerializer(qs, many=True).data
-       return Response(data)
+# using mixisn in class based api view
+# class ProductMixinView(mixins.ListModelMixin, generics.GenericAPIView):
     
-    if method == "POST":
-        serilizer = ProductsSerializer(request.data)
-        if serilizer.is_valid(raise_exception=True):
+#     queryset = Products.objects.all()
+#     serializer_class = ProductsSerializer
 
-             return Response(serilizer.data)
-        return Response({"invalid:" "not good data"}, status=400)
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+
+
+
+# functiion based view wher is implemanted creation and retrive
+# @api_view(["GET", "POST"])
+# def product_alt_view(request, pk=None, *args, **kwargs):
+#     method  = request.method
+
+#     if method == "GET":
+       
+#        if pk is not None:
+#            obj = get_object_or_404(Products, pk=pk)
+#            data = ProductsSerializer(obj).data
+#            return Response(data)
+       
+#        qs = Products.objects.all()
+#        data = ProductsSerializer(qs, many=True).data
+#        return Response(data)
+    
+#     if method == "POST":
+#         serilizer = ProductsSerializer(request.data)
+#         if serilizer.is_valid(raise_exception=True):
+
+#              return Response(serilizer.data)
+#         return Response({"invalid:" "not good data"}, status=400)
